0c323b53dee48b9bfebce4f0c9e9154b10d9e451
Viktoria <v1k0808@yandex.ru>
2022-12-12 23:39:15 +0300

    test2

--- test2/Program.cs
+++ test2/Program.cs
@@ -1,1 +1,21 @@
-﻿// git
+﻿//
+//
+//
+Console.WriteLine("Введите координату x1");
+int x1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату y1");
+int y1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату z1");
+int z1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координату x2");
+int x2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату y2");
+int y2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату z2");
+int z2 = int.Parse(Console.ReadLine()!);
+
+double d = Marth.Sqrt(Math.Pow(x2 - х1, 2) + Math.Pow(y2 - y1, 2) + (Math.Pow(z2 - z1, 2));
+Console.WriteLine($"расстояние между точками равно {d}");
+
+

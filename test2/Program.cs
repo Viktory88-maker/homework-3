bcb06c0cd077353af6f416c7d58d00a90b57a674
Viktoria <v1k0808@yandex.ru>
2022-12-13 00:28:40 +0300

    test3

--- test2/Program.cs
+++ test2/Program.cs
@@ -1,4 +1,4 @@
-﻿//Задача 21.Напишите программу, которая принимает на вход координаты
+//Задача 21.Напишите программу, которая принимает на вход координаты
 // двух точек и находит расстояние между ними в 3Д
 //пространстве
 Console.WriteLine("Введите координату x1");
@@ -15,7 +15,7 @@ int y2 = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите координату z2");
 int z2 = int.Parse(Console.ReadLine()!);
 
-double d = Marth.Sqrt(Math.Pow(x2 - х1, 2) + Math.Pow(y2 - y1, 2) + (Math.Pow(z2 - z1, 2));
+double d = Marth.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
 Console.WriteLine($"расстояние между точками равно {d}");
 
 
